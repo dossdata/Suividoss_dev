c07150da2745317303f02dbf918895394116ed2f
heryAndriam <hery0134@gmail.com>
2022-06-02 14:43:10 +0300

    modif ok 02-06-2022

--- admin/class/connect.php
+++ admin/class/connect.php
@@ -14,8 +14,6 @@ $port = 3306;
 try {
    //$dbo = new PDO("mysql:host=$server;dbname=$base", $root, $password);
    $dbo = new PDO('mysql:host='.$server.';port='.$port.';charset=utf8;dbname='.$base.'', $root, $password);
-   //$conn = new mysqli($server,$root,$password, $base);
-   //$con = new mysqli($server,$root,$password, $base);
   	$dbo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    	//echo "Connected successfully";
    }
